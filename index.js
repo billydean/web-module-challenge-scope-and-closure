477ef5334253d63d4f456493d7699063c1af3659
billydean <billy.goehring@gmail.com>
2022-01-18 09:53:54 -0800

    finished MVP tasks

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   1. What is the difference between counter1 and counter2?
   
   2. Which of the two uses a closure? How can you tell?
-  
+  counter1, because it declares "count" inside the function.
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +63,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    let inning = Math.floor(Math.random() * 3);
+    return inning;
 }
 
 
@@ -81,8 +83,21 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning,time){
+  let score = 0;
+  let final = {
+    };
+  let home = 0;
+  let away = 0;
+  for (let i=0; i<time; i++) {
+    score = inning();
+    home += score;
+    score = inning();
+    away += score;
+  }
+  final["Home"] = home;
+  final["Away"] = away;
+  return final;
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +105,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore() {
+  const score = {};
+  score.Home = inning();
+  score.Away = inning();
+  return score;
 }
 
 
